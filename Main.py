501ac55fcf9adbed204d083a13331a9e00ff7e50
hantyou <daniel.zhai@yahoo.com>
2020-02-17 23:13:50 +0800

    Update Function polished

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ import numpy as np
 
 import backgroundSubtractionFunctions as bgf
 
-filepath = r"D:\Programming\MATLAB\video_prog\MVI_1739.MOV"
+filepath = r"D:\Programming\MATLAB\video_prog\FroggerHighway.mp4"
 vid = cv2.VideoCapture(filepath)
 flag = vid.isOpened()
 if flag:
@@ -18,37 +18,85 @@ w = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
 h = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
 w = int(w)
 h = int(h)
+print((w, h))
 BG = np.zeros(shape=(frame.shape[0], frame.shape[1]), dtype=np.uint8)
 Sub = np.zeros(shape=(frame.shape[0], frame.shape[1]), dtype=np.uint8)
-a = 0.01
-cv2.namedWindow("Background", cv2.WINDOW_KEEPRATIO)
+cv2.namedWindow("Background_Show_Simple_Update", cv2.WINDOW_KEEPRATIO)
 cv2.namedWindow("Subtracted", cv2.WINDOW_KEEPRATIO)
 cv2.namedWindow("frame", cv2.WINDOW_KEEPRATIO)
-FrameNum = 0
-InitialFrN = 20
-InitialFrames = np.zeros((h, w, 3, InitialFrN))
-# PAR = np.zeros((int(h), int(w)))
-chn = 3
+a = 0.0025  # 简单更新的更新率
+FrameNum = 0  # 初始化帧计数器
+InitialFrN = 20  # 设置初始帧帧数
+chn = 4
+InitialFrames = np.zeros((h, w, chn, InitialFrN))  # 设置一个4维数组存放InitialFrN个三维色彩图像
+print('======正在创建参数存储区======')
 PAR = bgf.PixelPar((h, w, chn))
-print(type(PAR))
+print('======创建完毕======')
+"""
+    PAR.xxxx[chn, y, x, nModel]的四个参数含义分别为
+    chn: 当前色彩通道，分别对应B，G，R；
+    y，x分别为高和宽；
+    nModel：高斯分量的选择；
+    ——（只要在Main中一直选择0分量，那么只要在更新程序中一直将背景分量排序到第一就可以了）；
+"""
+# print(type(PAR))
+BG_Show = BG
+FrameInNdarray = np.array([[[[0] * 3] * w] * h] * chn)
 while 1:
+    """======帧计数器======"""
     FrameNum += 1
-    ret, frame = vid.read()
-    if FrameNum <= InitialFrN:  # 记录前30帧
-        InitialFrames[:, :, :, FrameNum - 1] = frame
-    if FrameNum == InitialFrN:  # 用前30帧的平均作初始化
+    """======获取当前帧======"""
+    ret, ColorFrame = vid.read()
+    """======创建色彩通道分开的帧======"""
+    GrayFrame = cv2.cvtColor(ColorFrame, cv2.COLOR_BGR2GRAY)
+    [B, G, R] = cv2.split(ColorFrame)
+    SelfFrame = [B, G, R, GrayFrame]
+    """======初始化部分======"""
+    if FrameNum < InitialFrN:  # 记录前30帧
+        for i in range(chn):
+            InitialFrames[:, :, i, FrameNum - 1] = SelfFrame[i]
+        continue
+    if FrameNum == InitialFrN:  # 用前InitialFrN帧的平均作初始化
         bgf.InitialPars(InitialFrames, PAR)
-        BG = PAR.miu[:, :, 1]
-    # CF = gray[:, :]
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    BG = cv2.addWeighted(BG, 1 - a, gray, a, 0)
-    # bgf.updateBackGround(background=BG, frame=frame, PAR=PAR)
-    Sub = cv2.absdiff(BG, gray)
-    Sub = cv2.threshold(Sub, 40, 255, type=cv2.THRESH_BINARY)
-    cv2.imshow('frame', frame)
-    cv2.imshow("Background", BG)
+        BG_Show = np.uint8(PAR.miu[1, :, :, 0])
+        """定义彩色背景"""
+        BG_B = PAR.miu[0, :, :, 0]
+        BG_G = PAR.miu[1, :, :, 0]
+        BG_R = PAR.miu[2, :, :, 0]
+        """-----------"""
+        ColorBG = cv2.merge([BG_B, BG_G, BG_R])
+        cv2.namedWindow("Color Background", cv2.WINDOW_KEEPRATIO)
+        cv2.imshow("Color Background", np.uint8(ColorBG))
+        BG = [BG_B, BG_G, BG_R, Sub]
+        PAR.ShowPar(x=639, y=479, chn=0)
+        PAR.ShowPar(x=639, y=479, chn=1)
+        PAR.ShowPar(x=639, y=479, chn=2)
+    """======初始化结束======"""
+    if FrameNum > InitialFrN:
+        BG = [BG_B, BG_G, BG_R, Sub]
+        #BG_B = cv2.addWeighted(np.uint8(BG_B), 1 - a, B, a, 0)
+        #BG_G = cv2.addWeighted(np.uint8(BG_G), 1 - a, G, a, 0)
+        #BG_R = cv2.addWeighted(np.uint8(BG_R), 1 - a, R, a, 0)
+        ColorBG = cv2.merge([BG_B, BG_G, BG_R])
+        cv2.imshow("Color Background", np.uint8(ColorBG))
+
+    for i in range(len(SelfFrame)):
+        for j in range(3):
+            FrameInNdarray[i, :, :, j] = SelfFrame[i]
+    """======升级背景======"""
+    BG_Show = cv2.addWeighted(BG_Show, 1 - a, GrayFrame, a, 0)
+    bgf.updateBackGround(BG=BG, frame=SelfFrame, FrameInNdarray=FrameInNdarray, PAR=PAR)
+    """======执行背景相减步骤======"""
+    DirectSub = cv2.absdiff(BG_Show, GrayFrame)
+    # Sub = bgf.GetSub(BG,frame)
+    """======执行二值化步骤======"""
+    Sub = cv2.threshold(DirectSub, 50, 255, type=cv2.THRESH_BINARY)
+    """======显示计算结果======"""
+    # print(PAR.weight[1,34, 1, :] )
+    cv2.imshow('frame', ColorFrame)
+    cv2.imshow("Background_Show_Simple_Update", BG_Show)
     cv2.imshow("Subtracted", Sub[1])
-    if cv2.waitKey(20) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 vid.release()
 cv2.destroyAllWindows()
--- backgroundSubtractionFunctions.py
+++ backgroundSubtractionFunctions.py
@@ -1,29 +1,114 @@
+import math
+
 import numpy as np
 
 
 class PixelPar:
     def __init__(self, size):
-        self.weight = np.zeros(size)
-        self.miu = np.zeros(shape=size, dtype=np.uint8)
-        self.sigma = np.zeros(size)
-        self.nGM = np.ones(shape=size, dtype=np.uint8)
+        size = list(size)
+        k = 0.5
+        [h, w, chn] = size
+        self.weight = np.array([[[[1 / 3] * 3] * w] * h] * chn)
+        self.miu = np.array([[[[0] * 3] * w] * h] * chn)
+        self.sigma = np.array([[[[1] * 3] * w] * h] * chn)
+        self.N = np.array([[[[1] * 3] * w] * h] * chn)
+        self.M = np.array([[[[1] * 3] * w] * h] * chn)
+        self.Z = np.array([[[[1] * 3] * w] * h] * chn)
+        self.nGM = np.array([[[3] * w] * h] * chn)
+
+    def ShowPar(self, x, y, chn):
+        weight = self.weight[chn, y, x, :]
+        miu = self.miu[chn, y, x, :]
+        sigma = self.sigma[chn, y, x, :]
+        N = self.N[chn, y, x, :]
+        M = self.M[chn, y, x, :]
+        Z = self.Z[chn, y, x, :]
+        n = self.nGM[chn, y, x]
+        c = ["B", "G", "R"]
+        print("权重：", str(weight))
+        print("均值：", str(miu))
+        print("方差：", str(sigma))
+        print("N: ", str(N))
+        print("M: ", str(M))
+        print("Z: ", str(Z))
+        print("当前色彩通道: ", c[chn])
+        """
+        self.miu = np.zeros(shape=size, dtype=list)
+        self.sigma = np.zeros(shape=size, dtype=list)
+        self.nGM = np.ones(shape=size, dtype=list)
+        """
 
 
-def updateBackGround(background, frame, PAR):
-    [w, h] = frame.shape
+class SinglePixelPar:
+    def __init__(self):
+        self.weight = [1 / 3, 1 / 3, 1 / 3]
+        self.miu = [0, 0, 0]
+        self.sigma = [0, 0, 0]
+        self.nGM = 1
 
+
+"""
+    PAR.xxxx[chn, y, x, nModel]的四个参数含义分别为
+    chn: 当前色彩通道，分别对应B，G，R；
+    y，x分别为高和宽；
+    nModel：高斯分量的选择；
+    ——（只要在Main中一直选择0分量，那么只要在更新程序中一直将背景分量排序到第一就可以了）；
+"""
+
+
+def updateBackGround(BG, frame, FrameInNdarray, PAR, ControlPar='控制参数暂时为空'):
+    def UpDateWithoutNowBG(frame, BG_B, BG_G, BG_R):
+        for i in range(len(frame)):
+            a = 1
+
+    """Control Parameters"""
+    UpBasedOnNowBG = 0
+    UpWithoutNowBG = 1 - UpBasedOnNowBG
+    """Imported Values"""
+    [h, w] = frame[0].shape  # Get the shape
+    [B, G, R] = frame[0:3]  # Get the Color Frame
+    S = frame[3]  # Get the light strength map
+    # Get the colored background[BG_B, BG_G, BG_R, BG_flag]
+    # ,and a matrix BG_flag as the indicator of Background Place
+    [BG_B, BG_G, BG_R, BG_flag] = BG
+    [weight, miu, sigma, N, M, Z] = [PAR.weight, PAR.miu, PAR.sigma, PAR.N, PAR.M, PAR.Z]
+    """Calculate P(L_t|I(x, y, t), @theta)"""
+    P = weight / (np.sqrt(2 * math.pi) * sigma) * np.exp(
+        -1 * (FrameInNdarray - miu) * (FrameInNdarray - miu) / 2 / (sigma * sigma))
+    N = N + P
+    M = M + P * FrameInNdarray
+    Z = Z + P * FrameInNdarray * FrameInNdarray
+    sumN = N
+    sumN[:, :, :, 0] = np.sum(sumN, 3)
+    sumN[:, :, :, 1] = np.sum(sumN, 3)
+    sumN[:, :, :, 2] = np.sum(sumN, 3)
+    weight = N / sumN
+    miu = M / N
+    sigma = Z / N - miu * miu
+
+    """Update Part"""
+    """
+    for j in range(h):
+        for i in range(w):
+            if UpWithoutNowBG:
+                UpDateWithoutNowBG(frame, BG_B, BG_G, BG_R)
+            else:
+                a = 0
+    """
 
 
 def InitialPars(frame, PAR):
+    print("正在初始化全图参数")
     [h, w, chn, t] = frame.shape
     print([w, h, chn])
-    sample = frame[:, :, 1, :]
-    PAR.miu[:, :, 1] = np.mean(sample, 2)
-    PAR.sigma[:, :, 1] = np.var(sample, axis=2)
-    PAR.weight = np.ones((h, w, chn))
-    """
-    for i in range(w):
-        for j in range(h):
-            weight = 1
-            PAR.weight[j, i, 1] = weight
-    """
+    for i in range(chn):
+        sample = frame[:, :, i, :]
+        PAR.miu[i, :, :, 0] = np.mean(sample, 2)
+        PAR.sigma[i, :, :, 0] = np.var(sample, axis=2)
+        PAR.M[i, :, :, :] = PAR.N[i, :, :, :] * PAR.miu[i, :, :, :]
+        PAR.Z[i, :, :, :] = PAR.N[i, :, :, :] * (PAR.sigma[i, :, :, :] + PAR.miu[i, :, :, :] * PAR.miu[i, :, :, :])
+    print("全图参数初始化完毕")
+
+
+def GetSub(BG, frame):
+    print("这里是获得背景差分结果的方程")
